3ed11e158df036ff73d9f71dbabfa6559ee7ca5e
Sasha Sushko <sushko@outlook.com>
2016-06-30 14:17:16 +0300

    Новые медиавыражения, PostCSS
    Обновил миксин с медиавыражениями, подключил PostCSS, подготовил почву
    для настройки линтера и перехода на CSSnext

--- gulpfile.js
+++ gulpfile.js
@@ -3,61 +3,74 @@
 // ToDo:
 // - Migrate to PostCSS with CSSnext
 // - Deploy
-// - Path to var
 
 // Plugins
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
 var spritesmith = require('gulp.spritesmith');
 var merge = require('merge-stream');
 
+
 // Build Styles
+var processors = [
+	autoprefixer({
+		browsers: ['last 3 version', 'IE >= 9']
+	})
+];
+
 gulp.task('styles', function () {
-	return gulp.src('./staticcontent/css/src/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass({outputStyle: 'nested'})
-			.on('error', sass.logError))
-		.pipe(autoprefixer({browsers: ['last 3 version', 'IE >= 9']}))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('./staticcontent/css'));
+  return gulp.src( './staticcontent/css/src/*.scss' )
+    .pipe( sourcemaps.init() )
+	.pipe( sass({outputStyle: 'expanded'} )
+		.on( 'error', sass.logError) )
+    .pipe( postcss(processors) )
+    .pipe( sourcemaps.write('.') )
+    .pipe( gulp.dest('./staticcontent/css') );
 });
 
+
 // Build JS
+var jsPath = [
+	'./js/lib/jquery-2.2.3.js',
+	'./js/src/checkJS.js',
+	'./js/src/checkTouch.js',
+	'./js/src/site.js'
+];
+
 gulp.task('js', function () {
-	return gulp.src([
-			'./js/lib/jquery-2.2.3.js',
-			'./js/src/checkJS.js',
-			'./js/src/checkTouch.js',
-			'./js/src/site.js'
-		])
-		.pipe(sourcemaps.init())
-		.pipe(concat('scripts.js'))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./js'));
+	return gulp.src( jsPath )
+		.pipe( sourcemaps.init() )
+		.pipe( concat('scripts.js') )
+		.pipe( sourcemaps.write() )
+		.pipe( gulp.dest('./js') );
 });
 
 // Build sprites
+var sprites = [
+	spritesmith({
+		retinaSrcFilter: './staticcontent/img/sprite/*@2x.png',
+		imgName: 'sprite.png',
+		retinaImgName: 'sprite@2x.png',
+		cssName: '_sprites.scss',
+		imgPath: '/staticcontent/img/sprite.png',
+		retinaImgPath:'/staticcontent/img/sprite@2x.png'
+	})
+]
+
 gulp.task('sprites', function () {
-	var spriteData = gulp.src('./staticcontent/img/icons/*.png')
-		.pipe(spritesmith({
-			retinaSrcFilter: './staticcontent/img/icons/*@2x.png',
-			imgName: 'sprite.png',
-			retinaImgName: 'sprite@2x.png',
-			cssName: '_sprites.scss',
-			imgPath: '/staticcontent/img/sprite.png',
-			retinaImgPath:'/staticcontent/img/sprite@2x.png'
-		}));
-	var imgStream = spriteData.img.pipe(gulp.dest('./staticcontent/img'));
-	var cssStream = spriteData.css.pipe(gulp.dest('./staticcontent/css/src/lib'));
-	return merge(imgStream, cssStream);
+	var spriteData = gulp.src( './staticcontent/img/sprite/*.png' )
+		.pipe( sprites );
+	var imgStream = spriteData.img.pipe( gulp.dest('./staticcontent/img') );
+	var cssStream = spriteData.css.pipe( gulp.dest('./staticcontent/css/src/lib') );
+	return merge( imgStream, cssStream );
 });
 
 // Watcher
 gulp.task('watch', function() {
-    gulp.watch('./js/**/*.js', ['js']);
-    gulp.watch('./staticcontent/css/src/**/*.scss', ['styles']);
-    gulp.watch('./staticcontent/img/icons/*.png', ['sprites']);
+    gulp.watch( './js/**/*.js', ['js'] );
+    gulp.watch( './staticcontent/css/src/**/*.scss', ['styles'] );
 });
