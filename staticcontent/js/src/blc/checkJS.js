f40868b9c136535ad0c78cba8d715523a974d072
Sasha Sushko <sushko@outlook.com>
2016-09-21 15:21:45 +0300

    Преход на v.2.0
    Удаляем старое, заменяем новым

--- staticcontent/js/src/blc/checkJS.js
+++
@@ -1,3 +0,0 @@
-// check JS
-var htmlTag = document.getElementsByTagName('html')[0];
-htmlTag.className = htmlTag.className.replace('no-js', 'is-js');
--- staticcontent/js/src/blc/checkTouch.js
+++
@@ -1,5 +0,0 @@
-// check touchscreen
-if('ontouchstart' in window || navigator.maxTouchPoints) {
-	var htmlTag = document.getElementsByTagName('html')[0];
-	htmlTag.className = htmlTag.className.replace('no-touch', 'is-touch');
-}
