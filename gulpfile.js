46510743ab2d9e0c928e6e7cd3e7f90117655855
Sasha Sushko <sushko@outlook.com>
2016-09-22 17:59:40 +0300

    Update to v.0.2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,35 @@
 'use strict';
 
-let gulp = require('gulp');
-let postcss = require('gulp-postcss');
-let sourcemaps = require('gulp-sourcemaps');
+let gulp = require('gulp'),
+	sourcemaps = require('gulp-sourcemaps'),
+	postcss = require('gulp-postcss'),
+	cssimport = require('postcss-import'),
+	cssnext = require('postcss-cssnext');
 
 gulp.task('css', () => {
-  let autoprefixer = require('autoprefixer');
-  let processors = [
-    autoprefixer({
-      browsers: [
-        'last 2 versions',
-        'Explorer >= 10',
-        'Android >= 4.4',
-        'last 2 ChromeAndroid versions',
-        'last 2 FirefoxAndroid versions',
-        'last 2 OperaMobile versions',
-        'last 2 OperaMini versions',
-        'last 2 Samsung versions',
-        'last 2 UCAndroid versions'
-      ]
-    })
-  ];
-  return gulp.src('./staticcontent/css/source/*.css')
-    .pipe(sourcemaps.init())
-    .pipe(postcss(processors))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./staticcontent/css'));
+	let browsers = [
+			'last 2 versions',
+			'Explorer >= 10',
+			'Android >= 4.4',
+			'last 2 ChromeAndroid versions',
+			'last 2 FirefoxAndroid versions',
+			'last 2 OperaMobile versions',
+			'last 2 OperaMini versions',
+			'last 2 Samsung versions',
+			'last 2 UCAndroid version'
+		],
+		features = [],
+		processors = [
+			cssimport,
+			cssnext({
+				browsers: browsers,
+				features: features
+			})
+		];
+
+	return gulp.src('./staticcontent/css/source/styles.css')
+		.pipe(sourcemaps.init())
+		.pipe(postcss(processors))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./staticcontent/css'));
 });
---
+++ readme.md
@@ -0,0 +1,24 @@
+## Duotek Frontend Template
+
+### ToDo
+
+- [x] Переход на [PostCSS](http://postcss.org/)
+	- [x] [cssnext](http://cssnext.io/)
+	- [x] [cssnext с ограничениями по функционалу](http://cssnext.io/usage/#features)
+	- [x] [сборка из @import](https://github.com/postcss/postcss-import)
+	- [x] [Autoprefixer](https://github.com/postcss/autoprefixer)
+- [ ] [Линтер стилей](http://stylelint.io/)
+- [ ] [Линтер скриптов](http://jshint.com/)
+- [ ] [Загрузка шрифтов](http://css-live.ru/articles/ischerpyvayushhee-rukovodstvo-po-strategiyam-zagruzki-veb-shriftov.html)
+- [ ] Вывод ошибок (?) [postcss-reporter](https://github.com/postcss/postcss-reporter) [postcss-browser-reporter](https://github.com/postcss/postcss-browser-reporter)
+- [ ] [Модернайзер](https://modernizr.com/)
+- [ ] Спрайты
+	- [ ] PNG
+	- [ ] SVG
+
+
+### Особенности
+
+##### Сборка
+
+##### Файловая структура
