a7aa25e8de0309bf98f5ae7e345a83d1ba3ce540
Sasha Sushko <sushko@outlook.com>
2017-03-21 14:09:29 +0300

    Adds IE9 support to JS check method

--- staticcontent/source/js/components/isJS.js
+++ staticcontent/source/js/components/isJS.js
@@ -1,3 +1,21 @@
-// Check JS support
-document.documentElement.classList.remove('no-js');
-document.documentElement.classList.add('js');
+/**
+ * Check JS support
+ * @returns {boolean}
+ */
+(function checkJSsupport() {
+	let html = document.documentElement;
+
+	// Check 'classList' method supports
+	if (html.classList) {
+		// If support, change indicator
+		html.classList.remove('no-js');
+		html.classList.add('js');
+
+		return true;
+	}
+
+	// If 'classList' not supports, change indicator by replacing
+	html.className = html.className.replace('no-js', 'js');
+
+	return true;
+})();
