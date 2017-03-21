7d0170690ae02f43caf80305e32626c5966cbaad
Sasha Sushko <sushko@outlook.com>
2017-03-21 14:56:06 +0300

    Rewrites JS support checker, because poly added

--- staticcontent/source/js/components/isJS.js
+++ staticcontent/source/js/components/isJS.js
@@ -3,19 +3,7 @@
  * @returns {boolean}
  */
 (function checkJSsupport() {
-	let html = document.documentElement;
-
-	// Check 'classList' method supports
-	if (html.classList) {
-		// If support, change indicator
-		html.classList.remove('no-js');
-		html.classList.add('js');
-
-		return true;
-	}
-
-	// If 'classList' not supports, change indicator by replacing
-	html.className = html.className.replace('no-js', 'js');
-
+	document.documentElement.classList.remove('no-js');
+	document.documentElement.classList.add('js');
 	return true;
 })();
