5f361413adfa753e684613a5961a20d3d527942a
Sasha Sushko <sushko@outlook.com>
2017-05-12 12:23:54 +0300

    Prepare code for release

--- staticcontent/source/js/app.js
+++ staticcontent/source/js/app.js
@@ -1,7 +1,7 @@
 /** Все методы и свойства клиентского JS на сайте */
 const APP = {
 	/**
-	 * Добавляет gul в APP
+	 * Добавляет компонент в APP
 	 * @param {object} component
 	 */
 	addComponent: function(component) {
